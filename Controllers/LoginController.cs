c08135eac0776b3513806659630ccf16936f4381
Andriele Joras dos Santos <andrielejorasdossantos@gmail.com>
2022-12-02 09:54:52 -0300

    :rocket: Final

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using API_Login.Data.Requests;
+using API_Login.Services;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Login.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private LoginService _loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            Result resultado = _loginService.Login(request);
+            if (resultado.IsFailed)
+            {
+                return Unauthorized(resultado.Errors.FirstOrDefault());
+            }
+            return Ok(resultado.Successes.FirstOrDefault());
+        }
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,35 @@
+﻿using API_Login.Data.Requests;
+using API_Login.Models;
+using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Login.Services
+{
+    public class LoginService
+    {
+        private SignInManager<IdentityUser<int>> _signInManager;
+        private TokenService _tokenService;
+
+        public LoginService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService)
+        {
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        public Result Login(LoginRequest loginRequest)
+        {
+            var logar = _signInManager.PasswordSignInAsync(loginRequest.Username, loginRequest.Password, false, false);
+            if (logar.Result.Succeeded)
+            {
+                var userIdentity = _signInManager
+                    .UserManager.Users
+                    .FirstOrDefault(usuario => usuario.NormalizedUserName == loginRequest.Username.ToUpper());
+
+                Token token = _tokenService.CreateToken(userIdentity);
+                return Result.Ok().WithSuccess(token.Value);
+            }
+            return Result.Fail("Login falhou");
+        } 
+    }
+}
